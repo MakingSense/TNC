318049ecf7c23d6aad34a150bfd00aecb91264e9
nazarenooviedo <noviedo@makingsense.com>
2016-08-17 16:16:09 -0300

    WIP on Map

---
+++ mapbox/data.js
@@ -0,0 +1,159 @@
+{
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#0000ff",
+            "marker-size": "medium",
+            "id": 2832,
+            "name": "Rietvlei Dam",
+            "type": "water_source"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.274, -25.878269]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-size": "medium",
+            "marker-color": "#0000ff",
+            "id": 2833,
+            "name": "Vaal Dam",
+            "type": "water_source"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.281897, -27.060757]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#0000ff",
+            "marker-size": "medium",
+            "id": 2834,
+            "name": "Vaal Barrage",
+            "type": "water_source"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.121464, -26.891455]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#0000ff",
+            "marker-size": "medium",
+            "id": 2835,
+            "name": "Local boreholes",
+            "type": "water_source"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.168121, -25.676188]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#0000ff",
+            "marker-size": "medium",
+            "id": 2864,
+            "name": "Katse Dam - Lesotho Highlands",
+            "type": "water_source"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.505445, -29.335421]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#0000ff",
+            "marker-size": "medium",
+            "id": 2865,
+            "name": "Mohale Dam - Lesotho Highlands",
+            "type": "water_source"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.093972, -29.456078]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#0000ff",
+            "marker-size": "medium",
+            "id": 2866,
+            "name": "Leeukraal Dam",
+            "type": "water_source"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.277221, -25.390617]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#0000ff",
+            "marker-size": "medium",
+            "id": 2867,
+            "name": "Roodeplaat Dam",
+            "type": "water_source"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.372235, -25.621774]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#ff8000",
+            "marker-size": "medium",
+            "id": 11,
+            "country_name": "South Africa",
+            "country_code": "ZAF",
+            "city_name": "Pretoria",
+            "type": "city",
+            "sources_count": 8,
+            "sources": []
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.18009331, -25.7435779]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#ff8000",
+            "marker-size": "medium",
+            "id": 11,
+            "country_name": "South Africa",
+            "country_code": "ZAF",
+            "city_name": "Johannesburg",
+            "type": "city",
+            "sources_count": 4,
+            "sources": []
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.0387369, -26.20524152]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#ff8000",
+            "marker-size": "medium",
+            "id": 11,
+            "country_name": "South Africa",
+            "country_code": "ZAF",
+            "city_name": "East Rand",
+            "type": "city",
+            "sources_count": 4,
+            "sources": []
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [28.346732, -26.178548]
+        }
+    }]
+}
